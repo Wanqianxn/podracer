75593fe1ee866cffc4ee8437cf0e659ec31a5b9c
wanqianxn <ywanqian95@gmail.com>
2018-06-19 00:32:56 -0700

    added port details to app.py

--- application.py
+++ application.py
@@ -125,3 +125,8 @@ def suggestions():
     suggestions = get_suggestions(client)
     ml_suggestions = clustering(client, session["username"])[:5]
     return render_template('suggestions.html', suggestions=suggestions, ml_suggestions=ml_suggestions)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
